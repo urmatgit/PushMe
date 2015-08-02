5d48756926ed43f4c54b06f1f31e31a53fda08f2
urmatgit <urmat2g@gmail.com>
2015-08-02 16:01:32 +0300

    automig

--- PushMe/PushMe/Migrations/Configuration.cs
+++ PushMe/PushMe/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace PushMe.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(PushMe.Models.ApplicationDbContext context)
